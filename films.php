168b707c0899e1dc40af72364e2e9fdde592eb9d
Besancon <besancon.julien@gmail.com>
2017-02-13 12:06:50 +0100

    test de recherche.php

---
+++ films.php
@@ -0,0 +1,28 @@
+<?php
+
+include 'vendor/autoload.php';
+echo"perdu";
+//on inclus le modele
+include 'recherche.php';
+echo"perdu2";
+try {
+    //indiquer le dossier ou on trouve les templates
+    $loader = new Twig_Loader_Filesystem('templates');
+    //initialiser l'environement Twig_loader_filesystem
+    $twig = new Twig_Environment($loader);
+
+    //recupérer les données depuis la base
+    $cont = new Recherche();
+    $films = $cont->get_all_films();
+
+    //charger le template
+    $template = $twig->loadTemplate('films.html');
+    $titre = "Liste des films ";
+    echo $template->render(array(
+      'titre'=>$titre,
+      'films'=> $films,
+    ));
+  }catch (Exception $e){
+    die('ERROR: '.$e->getMessage());
+  }
+?>
---
+++ recherche.php
@@ -0,0 +1,51 @@
+<?php
+
+require("connect.php");
+
+function connect_bd(){
+	$dsn="mysql:dbname=".BASE.";host=".SERVER;
+		try{
+		$connexion=new PDO($dsn,USER,PASSWD);
+		}
+		catch(PDOException $e){
+		printf("Échec de la connexion : %s\n", $e->getMessage());
+		exit();
+		}
+	return $connexion;
+}
+
+class Recherche{
+
+  static $connexion;
+
+  function __construct(){
+    $this->connexion = connect_bd()
+  }
+
+  function get_all_films(){
+    $resultat="erreur requete";
+    echo "teest";
+    $sql = "select * from films";
+    $stmt = $this->connexion->prepare($sql);
+    if($stmt->execute()){
+      $resultat=$stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    return $resultat;
+  }
+
+  function get_all_acteurs(){
+    $sql = "select * from individus";
+    $stmt = $this->connexion->prepare($sql);
+    $stmt->execute();
+    return $stmt->fetch(PDO::FETCH_OBJ);
+  }
+
+  function get_all_genres(){
+    $sql = "select * from genres";
+    $stmt = $this->connexion->prepare($sql);
+    $stmt->execute();
+    return $stmt->fetch(PDO::FETCH_OBJ);
+  }
+
+}
+?>
